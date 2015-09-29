6b7c10cbfb40cd8f98b192aedd77ac37bdb2a38c
Brock Oberhansley <brockoberhansley@Brocks-MacBook-Pro.local>
2015-09-29 16:03:14 -0600

    finished the lecture

--- AutolayoutExample/AutolayoutExample/AETableViewCell.m
+++ AutolayoutExample/AutolayoutExample/AETableViewCell.m
@@ -20,21 +20,60 @@ static const CGFloat margin = 10.0;
     [self.photoButton setTitle:@"Add" forState:UIControlStateNormal];
     [self.photoButton setTintColor:[UIColor whiteColor]];
     [self.photoButton setBackgroundColor:[UIColor blueColor]];
+    self.photoButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    [self.photoButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.contentView addSubview:self.photoButton];
     
     self.nameField = [UITextField new];
     self.nameField.frame = CGRectMake(margin + 48 + margin, 6, 125, 48);
     self.nameField.borderStyle = UITextBorderStyleRoundedRect;
+    [self.nameField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.contentView addSubview:self.nameField];
     
     self.scoreField = [UITextField new];
     self.scoreField.frame = CGRectMake(margin + 48 + margin + 125 + margin, 6, 55, 48);
     self.scoreField.borderStyle = UITextBorderStyleRoundedRect;
+    [self.scoreField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.contentView addSubview:self.scoreField];
     
     self.scoreStepper = [UIStepper new];
     self.scoreStepper.frame = CGRectMake(margin + 48 + margin + 125 + margin + 55 + margin, 15, 0, 0);
+    [self.scoreStepper setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.contentView addSubview:self.scoreStepper];
+    
+    NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_photoButton, _nameField, _scoreField, _scoreStepper);
+    
+    NSArray *constraints = [NSLayoutConstraint
+                            constraintsWithVisualFormat:@"h:|-[_phoroButton]-[_nameField(>=95)]-[_scoreField(>=45)]-(>=8)-[_scoreStepper]-|"
+                                                options:NSLayoutFormatAlignAllCenterY
+                                                metrics:nil
+                                                  views:viewDictionary];
+    [self.contentView addConstraints:constraints];
+    
+    NSLayoutConstraint *oneConstraint = [NSLayoutConstraint constraintWithItem:self.scoreField
+                                                                     attribute:NSLayoutAttributeCenterX
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.contentView
+                                                                     attribute:NSLayoutAttributeCenterX
+                                                                    multiplier:1.0
+                                                                      constant:0.0];
+    [self.contentView addConstraint:oneConstraint];
+    
+    constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"v:|-[_photoButton]-|"
+                                                          options:0
+                                                          metrics:nil
+                                                            views:viewDictionary];
+    [self.contentView addConstraints:constraints];
+    
+    oneConstraint = [NSLayoutConstraint constraintWithItem:self.photoButton
+                                              attribute:NSLayoutAttributeHeight
+                                              relatedBy:NSLayoutRelationEqual
+                                                 toItem:self.photoButton
+                                              attribute:NSLayoutAttributeWidth
+                                             multiplier:1.0
+                                               constant:0.0];
+    
+    [self.contentView addConstraint:oneConstraint];
     
     return self;
 }
